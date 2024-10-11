2c399873bc174bdb5282a3991a978b26acd73254
Jessica Murphy <murphyjes@thehutgroup.com>
2024-10-11 18:17:08 +0100

    convert indents to tabs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,45 +6,45 @@
  */
 
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
-    checkstyle
+	// Apply the application plugin to add support for building a CLI application in Java.
+	application
+	checkstyle
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+	// Use Maven Central for resolving dependencies.
+	mavenCentral()
 }
 
 dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation(libs.junit.jupiter)
+	// Use JUnit Jupiter for testing.
+	testImplementation(libs.junit.jupiter)
 
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    // This dependency is used by the application.
-    implementation(libs.guava)
+	// This dependency is used by the application.
+	implementation(libs.guava)
 
-    checkstyle("io.spring.javaformat:spring-javaformat-checkstyle:0.0.43")
+	checkstyle("io.spring.javaformat:spring-javaformat-checkstyle:0.0.43")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
 }
 
 application {
-    // Define the main class for the application.
-    mainClass = "org.example.App"
+	// Define the main class for the application.
+	mainClass = "org.example.App"
 }
 
 checkstyle {
-    toolVersion = "9.3"
+	toolVersion = "9.3"
 }
 
 tasks.named<Test>("test") {
-    // Use JUnit Platform for unit tests.
-    useJUnitPlatform()
+	// Use JUnit Platform for unit tests.
+	useJUnitPlatform()
 }

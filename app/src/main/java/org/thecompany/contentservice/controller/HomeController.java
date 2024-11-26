2e2954daa37a5d8d3e659c55818dbed554e609fc
Jessica Murphy <murphyjes@thehutgroup.com>
2024-11-26 15:39:18 +0000

    update HomeController to be a REST controller

--- app/src/main/java/org/thecompany/contentservice/controller/HomeController.java
+++ app/src/main/java/org/thecompany/contentservice/controller/HomeController.java
@@ -16,15 +16,14 @@
 
 package org.thecompany.contentservice.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
 	@GetMapping("/")
-	public @ResponseBody String greeting() {
+	public String greeting() {
 		return "Hello, World";
 	}
 
